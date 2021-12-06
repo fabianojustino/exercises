4227f0f34f73660b453190c277763cb5d0790b7b
fabianojustino <fibianojustino@gmail.com>
2021-12-06 09:05:23 -0300

    alterado color e background

---
+++ 5_1/script.js
@@ -0,0 +1,10 @@
+let title = document.querySelector("h1");
+title.innerText = "Indiana Jones"
+
+let tasks = document.querySelectorAll("div");
+
+
+let secondP = document.querySelectorAll("h3");
+secondP[1].style.backgroundColor= "yellow";
+secondP[1].style.color = "black"
+
